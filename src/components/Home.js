e4dbb51eec23fe4737a48fdc2c14272fec69953b
HilSny <hillary.snyder5@gmail.com>
2016-10-17 15:24:29 -0500

    Added Home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {Column, Row} from 'react-foundation'
+
+const Home = () => (
+  <section>
+
+    <Row>
+      <Column small={12} medium={8} large={6} centerOnMedium centerOnLarge>
+        Hello!
+      </Column>
+    </Row>
+  </section>
+)
+
+export default Home
