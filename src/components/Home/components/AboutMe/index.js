5ef79a306034397a2054555f4f269b5bf996c477
HilSny <hillary.snyder5@gmail.com>
2016-10-18 11:15:55 -0500

    Adds about me

--- src/components/Home/components/AboutMe/index.js
+++ src/components/Home/components/AboutMe/index.js
@@ -3,23 +3,31 @@ import React from 'react'
 import {Column, Row} from 'react-foundation'
 
 const styles={
+  about: {
+    fontSize: 20,
+  },
   aboutme: {
-    maxWidth: 800,
-    textAlign: 'center',
+    fontFamily: 'Helvetica',
+    maxWidth: 600,
+    margin: 'auto',
+  },
 
-  }
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+  },
 }
 
 const AboutMe = () => (
   <div style={styles.aboutme}>
     <Row>
       <Column small={12} centerOnSmall>
-        <h1 >About Me</h1>
+        <h1 style={styles.title} >About Me</h1>
       </Column>
     </Row>
     <Row>
       <Column small={12}>
-        <p className="text-center">My name is Hillary Snyder. I am a developer in the Chicago area who is interested in ReactJS, Women in Tech, Coffee, Running and Beer.</p>
+        <p style={styles.about} className="text-center">My name is Hillary Snyder. I am a developer in the Chicago area who is interested in ReactJS, Women in Tech, Coffee, Running and Beer.</p>
       </Column>
     </Row>
   </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,15 +5,28 @@ import {Column, Row} from 'react-foundation'
 import AboutMe from './components/AboutMe'
 
 const styles={
-
+  container: {
+    margin: 15,
+  },
+  about: {
+    border: '1px Solid black',
+    margin: 'auto',
+    maxHeight: 800,
+    maxWidth: 1000,
+    minHeight: 800,
+    minWidth: 100,
+  }
 }
 
 const Home = () => (
-  <section>
+  <section style={styles.container}>
 
     <Row>
-      <Column small={12}>
-        <AboutMe />
+      <Column small={12} centeronSmall>
+        <div style={styles.about}>
+          <AboutMe />
+        </div>
+
       </Column>
     </Row>
   </section>
